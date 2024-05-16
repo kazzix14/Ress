7850bfeee91430ba9223ad7ad07ae7c9ebc7d07e
kazzix <kazzix14@gmail.com>
2024-05-16 17:20:14 +0900

    add

--- client/browser.rb
+++ client/browser.rb
@@ -4,14 +4,46 @@ require '/bundle/setup'
 require 'js'
 require 'slim'
 
-def set_count(count)
-  @count = count
+def write
+  @during_write = true
+  yield
+  @during_write = false
 
   render
 end
 
-def count
-  @count || 0
+def use_state(name, initial_value)
+  instance_variable_set("@#{name}", initial_value) unless instance_variable_defined?("@#{name}")
+
+  define_method(name) do
+    instance_variable_get("@#{name}")
+  end
+
+  define_method("set_#{name}") do |value|
+    raise 'must be inside dirty' unless @during_write
+
+    instance_variable_set("@#{name}", value)
+  end
+end
+
+def render_internal(context)
+  JS.global[:document][:documentElement][:innerHTML] = Slim::Template.new { template }.render(context)
+end
+
+def validity
+  (count % 3).zero?
+end
+
+def handle_click
+  write do
+    set_count count + 1
+  end
+end
+
+def handle_click2
+  write do
+    set_word JS.global.fetch('/random_word').await.text.await
+  end
 end
 
 def template
@@ -21,8 +53,14 @@ def template
       head
         title Slim Template
       body
-        h1 Hello from Slim! 🫰🏻
-        button[onclick='window.rubyVM.eval("set_count(count + 1)");'] くっりくしてね
+        h1[style="background-color: #{validity ? 'green' : 'red'}"] Hello from Slim! 🫰🏻
+        p = word
+        - if validity
+          p 3の倍数
+        - else
+          p 3の倍数ではない
+        button[onclick='window.reval("handle_click()");'] くっりくしてね
+        button[onclick='window.reval("handle_click2()");'] くっりくしてね(API)
         p
           span くっりくした回数:
           span = count
@@ -31,8 +69,10 @@ def template
 end
 
 def render
-  html_output = Slim::Template.new { template }.render(self)
-  JS.global[:document][:documentElement][:innerHTML] = html_output
+  use_state(:count, 0)
+  use_state(:word, '')
+
+  render_internal(self)
 end
 
 render
